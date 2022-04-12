de4abd7ad603fca043234c1443c3bb10b8cd9181
thomasZhang98 <zhangjiayang@northeastern.edu>
2022-04-12 15:34:18 -0400

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitController from "./controllers/tuits-controller.js";
-const CONNECTION_STRING = 'mongodb://localhost:27017/webdev'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev'
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.listen(process.env.PORT || 4000);
